f99be296509805ce18ed6e8ccd2477dd73d5268f
AW528286 <113694026+AW528286@users.noreply.github.com>
2023-01-24 16:45:51 +0000

    enemy spawner fixed

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -20,6 +20,6 @@ public class EnemySpawner : MonoBehaviour
     {
         yield return new WaitForSeconds(interval);  
         GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6f), 0), Quaternion.identity);
-        StartCoroutine(spawnEnemy(interval, newEnemy));
+        StartCoroutine(spawnEnemy(interval, enemy));
     }
 }
